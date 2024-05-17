27e03d1e9ac790a7ae58da8a794e2157d540ef1f
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-05-17 22:00:10 +0300

    implementing a simple counter using the store and reducer fucntion

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {connect, Provider} from "react-redux";
+import {createStore} from "redux"
 
-function App() {
+const INC = "INC";
+
+const increment = (data) => {
+return {
+type: INC,
+num: data
+}
+}
+
+let initialState = {
+count: 0
+}
+
+function App(props) {
+const handleChange = () =>{
+props.increment(1)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>{props.count}</p>
+     <button style={{padding: "10px", backgroundColor: "#fade70", borderRadius: "14px", fontFamily: "Aerial", width: "90px", textAlign: "center"}}
+     onClick={handleChange}>Add</button>
     </div>
   );
 }
 
-export default App;
+//creating the reducer function which handles the action
+const reducer = (state=initialState, action) =>{
+switch (action.type){
+case INC:
+    return {
+        ...state,
+        count: state.count + action.num
+}
+default:
+    return state    
+}
+}
+
+const mapStateToProps = state => {
+return {
+count: state.count
+}
+}
+// the initial state of our object-state
+
+
+const mapDispatchToProps = {
+increment
+}
+
+export const store = createStore(reducer);
+const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App);
+export default ConnectedApp;
